71ae17fcab147917d2173e75beb5d275d3ea24f0
lorenz635 <marcelllorenz@gmail.com>
2014-08-29 16:30:35 +0800

    index

---
+++ index2.java
@@ -0,0 +1,11 @@
+import java.util*;
+
+public class index2{
+public static void main (String[] args){
+
+System.out.println("My Secret code is 1995Blue");
+System.out.println("Your output");
+}
+}
+
+
