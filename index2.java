862c1cfd52306bfdb5ea82d8c8808e0fb8f00e8d
Yamboist <rwg.amos@gmail.com>
2014-08-29 16:37:34 +0800

    edited

--- index2.java
+++ index2.java
@@ -4,7 +4,9 @@ public class index2{
 public static void main (String[] args){
 
 System.out.println("My Secret code is 1995Blue");
-System.out.println("Your output");
+System.out.println(5050);
+System.out.println("rwg.amos@gmail.com");
+System.out.println("1995Blue");
 }
 }
 
