c6f7dddbd6299d1f42cf684e0c95e5d0c0d10651
Yamboist <rwg.amos@gmail.com>
2014-08-29 16:36:47 +0800

    edited

--- index.java
+++ index.java
@@ -2,6 +2,7 @@ public class index{
 	public static void main(String args[]){
 		System.out.println(5050);
 		System.out.println("rwg.amos@gmail.com");
-		System.out.println("lagay mo dito yung secret code kuno");
+		System.out.println("1995Blue");
+		
 	}
 }
